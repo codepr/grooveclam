cdad2038629382c29ba6a573a7437fe25c9f7cc7
Andrea <a.g.baldan@gmail.com>
2015-05-18 23:56:26 +0200

    ajax

--- views/user/show.php
+++ views/user/show.php
@@ -6,9 +6,9 @@
 if(isset($_SESSION['logged'])) {
 	if($_SESSION['uid'] != $_GET['id']) {
 		if(in_array($_GET['id'], $fellows)) {
-			echo "<p><button class='exit add'><a href='#'>&#10010 Follow</a></button></p>";
+			echo "<p><button class='exit add'><a href='?controller=user&action=follow&id=".$_GET['id']."'>&#10010 Follow</a></button></p>";
 		} else {
-			echo "<p><button class='exit add'><a href='#'>&#10010 Unfollow </a></button></p>";
+			echo "<p><button class='exit add'><a href='?controller=user&action=unfollow&id=".$_GET['id']."'>&#10010 Unfollow </a></button></p>";
 		}
 	}
 }
