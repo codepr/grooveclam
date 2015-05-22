e827eb8d26d9599663b7c708bb1e1b8518f4cf88
Andrea <a.g.baldan@gmail.com>
2015-05-22 21:13:05 +0200

    6 queries

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,5 +1,6 @@
 DROP TRIGGER IF EXISTS checkDuration;
 DROP TRIGGER IF EXISTS errorTrigger;
+DROP TRIGGER IF EXISTS checkFollower;
 
 DELIMITER $$
 
@@ -8,7 +9,7 @@ BEFORE INSERT ON `Brani`
 FOR EACH ROW
 BEGIN
 IF(NEW.Durata < 0) THEN
-    CALL RAISE_ERROR('Song duration cannot be negative');
+    CALL RAISE_ERROR('La durata di un brano non può essere negativa');
 END IF;
 END $$
 
@@ -19,4 +20,16 @@ BEGIN
     SET NEW = NEW.errore;
 END $$
 
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER checkFollower
+BEFORE INSERT ON `Seguaci`
+FOR EACH ROW
+BEGIN
+    IF NEW.IdUtente = NEW.IdSeguace THEN
+       CALL RAISE_ERROR('Un utente non può seguire se stesso (IdUtente e IdSeguace devono essere diversi fra loro)');
+    END IF;       
+END $$
 DELIMITER ;
