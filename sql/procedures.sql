7a46d28b8e30e4936089f83c9c0a0085a89a8e0e
baldaz <andreagiacomo.baldan@studenti.unipd.it>
2015-05-25 16:01:43 +0200

    cursor

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -27,6 +27,36 @@ SELECT count(b.Genere) INTO Total FROM Brani b;
 INSERT INTO Distribution (Genere, Percentuale)
 SELECT Genere, CONCAT(FLOOR((count(Genere) / Total) * 100), "%")
 FROM Brani GROUP BY Genere;
+SELECT * FROM `Distribution`;
+DROP TABLE `Distribution`;
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE GENRE_DISTRIBUTION_ALT(IN IdUser INT)
+BEGIN
+DECLARE Total INT DEFAULT 0;
+SELECT COUNT(b.IdBrano) INTO Total
+FROM Brani b INNER JOIN BraniCollezione bc ON(b.IdBrano = bc.IdBrano)
+                         INNER JOIN Collezioni c ON(bc.IdCollezione = c.IdCollezione)
+WHERE c.IdUtente = IdUser;
+DROP TEMPORARY TABLE IF EXISTS `Distribution`;
+CREATE TEMPORARY TABLE `Distribution` (
+       `Genere` VARCHAR(100),
+       `Percentuale` VARCHAR(6)
+) ENGINE=InnoDB;
+DECLARE CUR_Dist CURSOR FOR
+        SELECT b.Genere, COUNT(b.IdBrano)
+        FROM Brani b INNER JOIN BraniCollezione ON (b.IdBrano = bc.IdBrano)
+                                 INNER JOIN Collezioni c ON(c.IdCollezione = bc.IdCollezione)
+        WHERE c.IdUtente = IdUser;
+OPEN CUR_Dist
+REPEAT
+        FETCH CUR_Dist INTO
+        UNTIL Done END REPEAT;
+CLOSE CUR_Dist;
 END $$
 
 DELIMITER ;
