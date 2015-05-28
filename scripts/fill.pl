99be6d5fc654acd34fd23ec2d36ca213a915a7cb
Andrea <a.g.baldan@gmail.com>
2015-05-28 19:45:13 +0200

    covers..

--- scripts/fill.pl
+++ scripts/fill.pl
@@ -6,7 +6,7 @@ use DBI;
 my $file1 = "albumlist";
 my $file2 = "songs";
 my $database = 'abaldan-PR';
-my $hostname = 'basidati1004.studenti.math.unipd.it';
+my $hostname = 'basidati.studenti.math.unipd.it';
 my $port = 3306;
 my $user = 'abaldan';
 my $password = 'NtQEfO78';
@@ -16,17 +16,21 @@ open(my $ah, '<:encoding(UTF-8)', $file1);
 my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
 my $dbh = DBI->connect($dsn, $user, $password);
 my $drh = DBI->install_driver("mysql");
-my @databases = DBI->data_sources("mysql");
+#my @databases = DBI->data_sources("mysql");
 
 while(my $row = <$ah>) {
     chomp $row;
     my @fields = split(",", $row);
-    my $sth = $dbh->do('INSERT INTO Album (IdAlbum, Titolo, Autore, Info, Anno, Live, Locazione, PathCopertina) VALUES(?,?,?,?,?,?,?,?)', undef, $fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], "NULL", "NULL");
+    my $location = "NULL";
+    if(defined($fields[7])) {
+        $location = $fields[7];
+    }
+    my $sth = $dbh->do('INSERT INTO Album (IdAlbum, Titolo, Autore, Info, Anno, Live, Locazione, PathCopertina) VALUES(?,?,?,?,?,?,?,?)', undef, $fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $location, "NULL");
 }
 open(my $sh, '<:encoding(UTF-8)', $file2);
 while(my $row = <$sh>) {
     chomp $row;
-    my @fields = split(",", $row);
+    my @fields = split("   ", $row);
     my @duration = split(":", $fields[2]);
     my $drt = ($duration[0] * 60) + $duration[1];
     my $sth = $dbh->do('INSERT INTO Brani (IdAlbum, Titolo, Durata, Genere)VALUES(?,?,?,?)', undef, $fields[0], $fields[1], $drt, $fields[3]);
---
+++ scripts/scrape.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use LWP::UserAgent;
+use HTML::TableExtract;
+
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+$ua->env_proxy;
+
+my $file = shift or die "Usage: $0 filepath";
+open(my $fh, '<:encoding(UTF-8)', $file) or die "Could not open file '$file' $!";
+my $output = 'songs';
+open(my $oh, '>', $output) or die "Could not open file '$output' $!";
+while(my $r = <$fh>) {
+    chomp $r;
+    my @cols = split(",", $r);
+    $cols[1] =~ s/" "/"_"/g;
+    my $response = $ua->get('http://en.wikipedia.org/wiki/'.$cols[1].'#Track_listing');
+    if ($response->is_success) {
+        if($response->decoded_content =~ m/disambigbox/ ) {
+            $response = $ua->get('http://en.wikipedia.org/wiki/'.$cols[1].'_(album)#Track_listing');
+        }
+        my $url = "";
+        if($response->decoded_content =~ m{src="(.*?jpg)"}g) {
+            $url = $1;
+        }
+        my $te = HTML::TableExtract->new(headers => ['No.', 'Title', 'Length']);
+        $te->parse($response->decoded_content);
+        print "\n\t------------------------------------------------------------\n";
+        print "\t Album: $cols[1]\tArtist: $cols[2]";
+        print "\n\t------------------------------------------------------------\n";
+        print "\t Tracks:\n\n";
+        foreach my $ts ($te->table_states) {
+            foreach my $row ($ts->rows) {
+                if (defined $row->[1]) {
+                    next unless $row->[1] =~ /\w/;
+                    next if $row->[2] =~ m/\240/ || $row->[2] =~ m/ /;
+                    $row->[1] = $1 if $row->[1] =~ /"(.*?)"/;
+                    print $oh $cols[0]."   ".$row->[1]."   ".$row->[2]."   ".$cols[2]."\n";
+                    print "\t  $row->[1]\t\t$row->[2]\t\t$cols[2]\n";
+                }
+            }
+        }
+    }
+    print "\n";
+}
+print "\t** DONE **\n\n";
+close($output);
